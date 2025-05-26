d71ed35739778c60aa4e9b7791f9f1e388a9eee6
Guilherme Quirino <guilhermequirinocruz@gmail.com>
2025-05-26 19:54:10 -0300

    cli e ajustes no backend

--- src/backend.py
+++ src/backend.py
@@ -2,41 +2,17 @@ import os
 from pathlib import Path
 import platform
 
-import pytubefix
 from pytubefix import YouTube, Playlist
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_merge_video_audio
 
-DEFAULT_DOWNLOAD_PATH = '/var/home/guilhermequirino/Músicas'
+# DEFAULT_DOWNLOAD_PATH = '/var/home/guilhermequirino/Músicas'
+INVALID_CHARACTERS = ',.;:\'\'*|#</>\\?'
 TEMP_OUTPUT_PATH = './.temp'
 
-# código original: https://dev.to/stokry/download-youtube-video-to-mp3-with-python-26p
-
-# Não funciona porque o método Playlist só retorna os vídeos disponíveis
-# def verificar_integridade_playlist(url):
-#     playlist_verificar = Playlist(url)
-
-#     tamanho_playlist = len(playlist_verificar.video_urls)
-#     videos_disponiveis = tamanho_playlist
-#     print(f'Tamanho: {tamanho_playlist}')
-#     if tamanho_playlist == 0:
-#         print('Playlist vazia')
-#         return
-
-#     for url_video_verificar in playlist_verificar.video_urls:
-#         video = YouTube(url = url_video_verificar)
-#         try:
-#             video.check_availability()
-#         except pytube.exceptions.VideoUnavailable:
-#             print(f'Vídeo indisponível: {url_video_verificar}')
-#             videos_disponiveis -= 1
-
-#     porcentagem_disponiveis = (videos_disponiveis / tamanho_playlist) * 100
-#     print(f'Integridade: {videos_disponiveis} / {tamanho_playlist} ({porcentagem_disponiveis}%)')
+# Código original: https://dev.to/stokry/download-youtube-video-to-mp3-with-python-26p
 
 # Abre a pasta em que os arquivos foram baixados
-
-
 def open_destination_folder(path):
     match platform.system():
         case 'Linux':
@@ -49,8 +25,7 @@ def open_destination_folder(path):
 
 # Remove os caracteres que interferem no salvamento dos arquivos
 def remove_invalid_characters(name):
-    invalid_characters = ',.;:\'\'*|#</>\\?'
-    for character in invalid_characters:
+    for character in INVALID_CHARACTERS:
         name = name.replace(character, '')
 
     return name
@@ -78,7 +53,7 @@ def add_index_to_output_path(path, file_name, extension):
 def add_index_to_folder_path(path):
     if (not os.path.exists(path)):
         return path
-    
+
     path = add_index_to_output_path(path, '', '')
     # Remove o último '/' do caminho da pasta
     # https://stackoverflow.com/questions/2556108/rreplace-how-to-replace-the-last-occurrence-of-an-expression-in-a-string/59082116#59082116
@@ -94,7 +69,7 @@ def show_download_progress(stream, _, remaining_bytes):
 
     # print('Progresso: %.2lf/%.2lf MB (%.2lf%%)' % (remaining_bytes_mb, tamanho_stream_mb, porcentagem_download))
     print(
-        f'Progresso: {remaining_bytes_mb:.2f}/{download_percentage:.2f} MB ({download_percentage:.2f}%)')
+        f'Progresso: {remaining_bytes_mb:.2f}/{stream_size_mb:.2f} MB ({download_percentage:.2f}%)')
 
 
 # Exibe uma mensagem para downloads bem sucedidos
@@ -114,26 +89,50 @@ def download_stream(video, audio_only=False):
     return stream.download(output_path=TEMP_OUTPUT_PATH)
 
 
-# Verifica a disponibilidade do vídeo
-def is_video_available(yt):
+def is_url_valid(url, is_playlist):
     try:
-        yt.check_availability()
-    except pytubefix.exceptions.VideoUnavailable:
+        if is_playlist:
+            playlist = Playlist(url=url)
+            return is_playlist_available(playlist)
+
+        video = YouTube(url=url)
+        return is_video_available(video)
+    except:
+        return False
+
+
+# Verifica a disponibilidade do vídeo
+def is_video_available(video):
+    try:
+        video.check_availability()
+    except:
         return False
 
     return True
 
+# Verifica se a playlist está vazia
+
+
+def is_playlist_available(playlist):
+    playlist_length = 0
+    try:
+        playlist_length = playlist.length
+    except:
+        return False
+
+    return playlist_length != 0
+
 
 # Baixa e salva o arquivo
-def save_output(yt, path, audio_only):
+def save_output(video, path, audio_only):
     if audio_only:
-        audio_stream = download_stream(yt, True)
+        audio_stream = download_stream(video, True)
 
         # Salva o áudio renomeando o arquivo
         os.rename(audio_stream, path)
     else:
-        video_stream = download_stream(yt, False)
-        audio_stream = download_stream(yt, True)
+        video_stream = download_stream(video, False)
+        audio_stream = download_stream(video, True)
 
         # Junta o vídeo e o áudio
         ffmpeg_merge_video_audio(
@@ -149,34 +148,34 @@ def delete_temp_file(file):
 
 
 # Recebe os parâmetros e realiza o download
-def download(url='', audio_only=False, path=DEFAULT_DOWNLOAD_PATH, ignore_duplicates=True, open_folder=True):
-    yt = YouTube(url=url,
-                 on_progress_callback=show_download_progress,
-                 on_complete_callback=download_finished)
+def download(url, path, audio_only=False, ignore_duplicates=True, open_folder=True):
+    video = YouTube(url=url,
+                    on_progress_callback=show_download_progress,
+                    on_complete_callback=download_finished)
 
-    if not is_video_available(yt):
+    if not is_video_available(video):
         print(f'Vídeo indisponível: {url}')
         return
 
     # Define a extensão do arquivo e o nome formatado
     extension = '.mp3' if audio_only else '.mp4'
-    formatted_title = get_formatted_name(yt.title)
+    formatted_title = get_formatted_name(video.title)
     file_path = Path(f'{path + '/' + formatted_title}{extension}')
 
     if ignore_duplicates and os.path.exists(file_path):
-        print(f'Vídeo já foi baixado : {yt.title}')
+        print(f'Vídeo já foi baixado : {video.title}')
         return
 
-    print(f'Baixando: {yt.title}')
+    print(f'Baixando: {video.title}')
 
     # Salva o arquivo na pasta temporária
     output_path = f'{TEMP_OUTPUT_PATH}/output{extension}'
-    save_output(yt, output_path, audio_only)
+    save_output(video, output_path, audio_only)
 
     # Adiciona um índice ao arquivo, caso necessário
     fixed_output_path = add_index_to_output_path(
         path, formatted_title, extension)
-    
+
     # Move o arquivo para o destino
     os.rename(output_path, fixed_output_path)
 
@@ -194,11 +193,6 @@ def download(url='', audio_only=False, path=DEFAULT_DOWNLOAD_PATH, ignore_duplic
         open_destination_folder(path)
 
 
-# Verifica se a playlist está vazia
-def is_playlist_empty(playlist):
-    return len(playlist.video_urls) == 0
-
-
 # Cria uma pasta para a playlist
 def create_playlist_folder(path):
     if not os.path.exists(path):
@@ -206,12 +200,12 @@ def create_playlist_folder(path):
 
 
 # Baixa a playlist, criando uma pasta e salvando cada arquivo
-def download_playlist(url, audio_only, create_folder, ignore_duplicates, path=DEFAULT_DOWNLOAD_PATH):
+def download_playlist(url, path, audio_only, ignore_duplicates, create_folder):
     playlist = Playlist(url)
 
-    if is_playlist_empty(playlist):
+    if not is_playlist_available(playlist):
         print('Não foi possível realizar o download')
-        print('Playlist vazia ou privada')
+        print('Link incorreto, playlist vazia ou privada')
         return
 
     formatted_playlist_title = get_formatted_name(playlist.title)
@@ -220,20 +214,13 @@ def download_playlist(url, audio_only, create_folder, ignore_duplicates, path=DE
         path += f'/{formatted_playlist_title}'
         path = add_index_to_folder_path(path)
         create_playlist_folder(path)
-    
+
     print(f'Baixando playlist: {playlist.title}')
 
     # Baixa cada vídeo da playlist
     for current, video in enumerate(playlist.videos, 1):
         print(f'Baixando vídeo {current} de {playlist.length}')
 
-        download(url=video.watch_url, audio_only=audio_only, path=path, ignore_duplicates=ignore_duplicates, open_folder=False)
+        download(video.watch_url, path, audio_only, ignore_duplicates, False)
 
     open_destination_folder(path)
-
-
-# download(url='https://www.youtube.com/watch?v=a8CwpGARAsQ',
-#          audio_only=True, path=DEFAULT_DOWNLOAD_PATH, ignore_duplicates=False, open_folder=True)
-
-download_playlist(
-    url='https://www.youtube.com/playlist?list=PLjo8TsMC7aERPpO9Qbl_shkQGeGMZUpDL', audio_only=True, create_folder=True, ignore_duplicates=False, path=DEFAULT_DOWNLOAD_PATH)
